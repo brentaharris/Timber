e29fb71e4c22038f2130ab3d78d604ed98cce71c
brentaharris <brentaustinharris@gmail.com>
2025-09-06 13:41:52 -0400

    Initial Timber Game commit

---
+++ Timber.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+using std::cout;
+
+int main()
+{
+	VideoMode vm(1920, 1080);
+	RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
+
+	Texture textureBackground;
+	textureBackground.loadFromFile("graphics/background.png");
+	Sprite spriteBackground;
+	spriteBackground.setTexture(textureBackground);
+	spriteBackground.setPosition(0, 0);
+
+	Texture textureTree;
+	textureTree.loadFromFile("graphics/tree.png");
+	Sprite spriteTree;
+	spriteTree.setTexture(textureTree);
+	spriteTree.setPosition(810,0);
+
+	Texture textureBee;
+	textureBee.loadFromFile("graphics/bee.png");
+	Sprite spriteBee;
+	spriteBee.setTexture(textureBee);
+	spriteBee.setPosition(0, 700);
+
+	bool beeActive = false;
+	float beeSpeed = 0.0f;
+
+	Texture textureCloud;
+	textureCloud.loadFromFile("graphics/cloud.png");
+	Sprite spriteCloud1;
+	Sprite spriteCloud2;
+	Sprite spriteCloud3;
+
+	spriteCloud1.setTexture(textureCloud);
+	spriteCloud2.setTexture(textureCloud);
+	spriteCloud3.setTexture(textureCloud);
+
+	spriteCloud1.setPosition(0, 0);
+	spriteCloud2.setPosition(0, 100);
+	spriteCloud3.setPosition(0, 200);
+
+	bool cloud1Active, cloud2Active, cloud3Active = false;
+	float cloud1Speed, cloud2Speed, cloud3Speed = 0.0f;
+
+	Clock clock;
+	
+
+	while (window.isOpen())
+	{
+		if (Keyboard::isKeyPressed(Keyboard::Escape))
+		{
+			window.close();
+		}
+
+		window.clear();
+
+		Time dt = clock.restart();
+
+		if (!beeActive)
+		{
+			srand((int)time(0));
+			beeSpeed = (rand() % 200) + 200;
+
+			srand((int)time(0) * 10);
+			float height = (rand() % 500) + 500;
+			spriteBee.setPosition(2000, height);
+			beeActive = true;
+		}
+		else
+		{
+			spriteBee.setPosition(
+				spriteBee.getPosition().x - (beeSpeed * dt.asSeconds()),
+				spriteBee.getPosition().y
+			);
+
+			if (spriteBee.getPosition().x < -100)
+			{
+				beeActive = false;
+			}
+		}
+
+
+		window.draw(spriteBackground);
+
+		window.draw(spriteCloud1);
+		window.draw(spriteCloud2);
+		window.draw(spriteCloud3);
+
+		window.draw(spriteTree);
+
+		window.draw(spriteBee);
+
+		window.display();
+	}
+
+	return 0;
+}
+
+
